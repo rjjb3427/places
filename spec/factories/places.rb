70698d3ed1d008e4d951f5b7afc3e919f011b9f3
Prelang Builder <builder@prelang.com>
2014-07-06 19:42:39 +0000

    Generated model Place with migration

---
+++ spec/factories/places.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :place do
+    name "MyText"
+    location "MyText"
+    author nil
+  end
+end
